89b9fa79df639073d3e9131785821f9c4fed6bbe
masonseiler <masonseiler1@gmail.com>
2018-05-29 11:57:52 -0400

    a

--- portfolio.js
+++ portfolio.js
@@ -5,6 +5,7 @@ canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
 
+/*
 c.fillStyle = 'rgba(255, 0, 0, 0.5)';
 c.fillRect(100, 100, 100, 100);
 c.fillStyle = 'rgba(0, 255, 0, 0.5)';
@@ -27,4 +28,60 @@ for(var i =0; 1 < 100; i++) {
     c.arc(x, y, 30, 0, Math.PI * 2, false);
     c.strokeStyle = 'blue';
     c.stroke();
-}
+} */
+
+    window.addEventListener('mousemove', 
+        function(event) {
+        
+        })
+
+    function circle(x, y, dx, dy, radius){
+        this.x = x;
+        this.y = y;
+        this.dx = dx;
+        this.dy = dy;
+        this.radius = radius;
+
+        this.draw = function() {
+            c.beginPath();
+            c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+            c.strokeStyle = 'blue';
+            c.stroke();
+            c.fill();
+        }
+        this.update = function() {
+            if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+                this.dx = -this.dx;
+            }
+            if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+                this.dy = -this.dy;
+            }
+            this.x += this.dx;
+            this.y += this.dy;
+
+            this.draw();
+        }
+    }
+
+    var cirlceArray = [];
+
+    for(var i = 0; i < 100; i++) {
+        var x = Math.random() * innerWidth;
+        var y = Math.random() * innerHeight;
+        var dx = (Math.random() - 0.5) * 8;
+        var dy = (Math.random() - 0.5) * 8;
+        var radius = 30;
+        cirlceArray.push(new circle(x, y, dx, dy, radius));
+    }
+    console.log(circleArray);
+
+    function animate() {
+        requestAnimationFrame(animate);
+        c.clearRect(0, 0, innerWidth, innerHeight);
+
+        for(var i = 0; i < circleArray.length; i++) {
+            circleArray[i].update();
+        }
+    }
+
+    animate();
